fd12948fa5be81f55f407e2fb086185905497d27
renzmallari <renzmallari777@yahoo.com>
2017-03-01 02:20:42 -0500

    cpp file modified .

--- Readme.md
+++ Readme.md
@@ -1,4 +1,7 @@
 # ADD function
-	- add function will add value .
-	- call by value function parameter .
+	- Adds value to the number using function.
+	- Display the difference of call by value and call by reference
+		- In terms of use .
+		- In terms of output .
+	- Conclusion using add by refernce will change the value of the variable from the calling program while calling by value will make the copy of passing varible use it but wont affact the value from the calling program .
 	..addValue.
--- addNumber.cpp
+++ addNumber.cpp
@@ -4,21 +4,34 @@ Jayson Mallari
 */
 #include <iostream>
 using namespace std;
-void addFive(int a, int b){
+void addFiveValue(int a, int b){
 
-	cout<<"Entering addFive function "<<endl;
+	cout<<"Entering addFive function \"ADD BY VALUE\""<<endl<<endl;
 	cout<<"A and B before adding 5: a = "<<a<<" : b = "<<b<<endl;
 	a+=5;
 	b+=5;
-	cout<<"A and B after adding 5: a = "<<a<<" : b = "<<b<<endl;
-	cout<<"Leaving addFive function "<<endl;
+	cout<<"A and B after adding 5: a = "<<a<<" : b = "<<b<<endl<<endl;
+	cout<<"Leaving addFive function \"ADD BY VALUE\""<<endl;
+}
+
+void addFiveReference(int& a, int& b){
+
+	cout<<"Entering addFive function \"ADD BY VALUE\""<<endl<<endl;
+	cout<<"A and B before adding 5: a = "<<a<<" : b = "<<b<<endl;
+	a+=5;
+	b+=5;
+	cout<<"A and B after adding 5: a = "<<a<<" : b = "<<b<<endl<<endl;
+	cout<<"Leaving addFive function \"ADD BY VALUE\""<<endl;
 }
 
 int main()
 {
 	int a=10 , b=20;
 	cout<<"A and B BEFORE calling the addFive function : a = "<<a<<" : b = "<<b<<endl;
-	addFive(a,b);
-	cout<<"A and B AFTER calling the addFive function : a = "<<a<<" : b = "<<b<<endl;
+	addFiveValue(a,b);
+	cout<<"A and B AFTER calling the addFive function : a = "<<a<<" : b = "<<b<< ",After using \"CALL BY VALUE\""<<endl;
+
+	addFiveReference(a,b);
+	cout<<"A and B AFTER calling the addFive function : a = "<<a<<" : b = "<<b<< ",After using \"CALL BY REFERENCE\""<<endl;
 return 0;
 }
